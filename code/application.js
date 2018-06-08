3a0815872a678dc43b8be66392fa6299368c47f7
NadjaUvA <nadjavanthoff@gmail.com>
2018-06-08 09:08:42 +0200

    update gauge

--- code/application.js
+++ code/application.js
@@ -301,5 +301,32 @@ window.onload = function() {
 
     RadarChart.draw("#radar_chart", data_radar.slice(0,2), mycfg);
 
+
+    var gauge1 = loadLiquidFillGauge("fillgauge1", 55);
+    var config1 = liquidFillGaugeDefaultSettings();
+    config1.circleColor = "#FF7777";
+    config1.textColor = "#FF4444";
+    config1.waveTextColor = "#FFAAAA";
+    config1.waveColor = "#FFDDDD";
+    config1.circleThickness = 0.2;
+    config1.textVertPosition = 0.2;
+    config1.waveAnimateTime = 1000;
+
+
+
+    // onclick="gauge1.update(NewValue());
+
+    // gauge1.on("click", function(d, i) {
+    //     d3.select(this).update(NewValue());
+    //   });
+
+    function NewValue(){
+      if(Math.random() > .5){
+          return Math.round(Math.random()*100);
+      } else {
+          return (Math.random()*100).toFixed(1);
+      }
+  }
+
   };
 };
